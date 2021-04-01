243c1b5ed0f38b002e1d96692e5e3a2e2a31f5aa
Blake Kvarfordt <luanikvarfordt@gmail.com>
2021-03-31 19:55:15 -0700

    done

--- __tests__/pages/Widgets.ts
+++ __tests__/pages/Widgets.ts
@@ -6,6 +6,7 @@ export class Widgets extends BasePage {
     super(options);
     this.url = "https://devmountain-qa.github.io/Automation-Basics/build/";
   }
+
   async splitEvensAndOdds(numbers: Array<number>) {
     await this.setInput(By.name("evenOddInput"), numbers.join(","));
     return this.click(By.name("evenOddButton"));
@@ -19,6 +20,7 @@ export class Widgets extends BasePage {
     results.odds = await this.getText(By.name("oddResults"));
     return results;
   }
+
   async setObjectFilter(filter: string) {
     await this.setInput(By.name("objectFilterInput"), filter);
     return this.click(By.name("objectFilterButton"));
@@ -26,13 +28,15 @@ export class Widgets extends BasePage {
   async getFilteredObjects() {
     return this.getText(By.name("objectFilterResults"));
   }
+
   async setNameFilter(filter: string) {
-    await this.setInput(By.name("nameFilterInput"), filter);
-    return this.click(By.name("nameFilterButton"));
+    await this.setInput(By.id("nameFilterInput"), filter);
+    return this.click(By.id("nameFilterButton"));
   }
   async getFilteredNames() {
-    return this.getText(By.name("nameFilterResults"));
+    return this.getText(By.name("nameFilterResults",));
   }
+
   async checkPalindrome(maybePalindrome: string) {
     await this.setInput(By.name("palindromeInput"), maybePalindrome);
     await this.click(By.name("palindromeButton"));
@@ -40,4 +44,13 @@ export class Widgets extends BasePage {
       (text) => text.split(" ")[1]
     );
   }
-}
+  async addNumbers(a: number, b: number) {
+    await this.setInput(By.name("sumInput2"), b);
+    return this.click(By.name("sumButton"));
+  }
+  async getSum() {
+    return this.getText(By.name("sumResults")).then((text) =>
+      parseFloat(text.split(" ")[1])
+    );
+  }
+};
