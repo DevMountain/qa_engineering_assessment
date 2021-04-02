6048e184d75182b2ae7ad1b74af548f9dc77fbff
ninaeltom <74694825+ninaeltom@users.noreply.github.com>
2021-04-02 13:15:34 -0500

    Add files via upload

--- __tests__/pages/Widgets.ts
+++ __tests__/pages/Widgets.ts
@@ -27,8 +27,8 @@ export class Widgets extends BasePage {
     return this.getText(By.name("objectFilterResults"));
   }
   async setNameFilter(filter: string) {
-    await this.setInput(By.name("nameFilterInput"), filter);
-    return this.click(By.name("nameFilterButton"));
+    await this.setInput(By.id("nameFilterInput"), filter);
+    return this.click(By.id("nameFilterButton"));
   }
   async getFilteredNames() {
     return this.getText(By.name("nameFilterResults"));
@@ -37,6 +37,14 @@ export class Widgets extends BasePage {
     await this.setInput(By.name("palindromeInput"), maybePalindrome);
     await this.click(By.name("palindromeButton"));
     return this.getText(By.name("palindromeResults")).then(
+      (text) => text.split(" ")[1]
+    );
+  }
+  async getSum(a: string, b: string) {
+    await this.setInput(By.name("sumInput1"), a);
+    await this.setInput(By.name("sumInput2"), b);
+    await this.click(By.name("sumButton"));
+    return this.getText(By.name("sumResults")).then(
       (text) => text.split(" ")[1]
     );
   }
