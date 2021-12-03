7a3d47265d6d99475412a1b3e7d77b4039c288d0
Conner Swanson <calm.calamity2@gmail.com>
2021-12-02 21:48:06 -0600

    challenges updated

--- __tests__/challenge2.test.ts
+++ __tests__/challenge2.test.ts
@@ -1,5 +1,7 @@
 import { Widgets } from "./pages/Widgets";
 import * as palindromes from "./assets/palindromes.json";
+import * as palindromesSet2 from "./assets/palindromesSet2.json";
+import * as notPalindromes from "./assets/notPalindromes.json";
 
 describe("palindrome testing", () => {
   let widget = new Widgets();
@@ -13,6 +15,18 @@ describe("palindrome testing", () => {
     test(`"${palindrome}" should be a palindrome`, async () => {
       let results = await widget.checkPalindrome(palindrome);
       expect(results).toContain("true");
+    });
+  });
+  palindromesSet2.forEach((palindrome) => {
+    test(`"${palindrome}" should be a palindrome`, async () => {
+      let results = await widget.checkPalindrome(palindrome);
+      expect(results).toContain("true");
+    });
+  });
+  notPalindromes.forEach((palindrome) => {
+    test(`"${palindrome}" should not be a palindrome`, async () => {
+      let results = await widget.checkPalindrome(palindrome);
+      expect(results).toContain("false");
     });
   });
 });
